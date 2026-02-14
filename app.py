55e98a649e7ddfcd338ebd2a8297855db3444927
AssiousOussama <o.assious@esisa.ac.ma>
2026-02-14 19:03:44 +0100

    update files git

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask + Jenkins!"
+    return "Hello from Flask + Jenkins! with hooks hhhh wa mii"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5010)
